9d41fdb86c12d2dc4ac88f6e484f69f40776ce64
Dips2705 <rganguly143@gmail.com>
2019-08-05 16:53:46 +0530

    Lab 2 Assignment submitted

---
+++ Lab2 5-8-19/lab2.cpp
@@ -0,0 +1,143 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Point{
+	int x,y;
+	struct Point* next;
+};
+
+struct Point *head=NULL;
+
+void AddFirst(int x,int y)
+{
+	struct Point *temp=(struct Point*)malloc(sizeof(struct Point));
+	if(temp==NULL)
+	{
+		cout<<-1<<endl;
+		return;
+	}
+	temp->x=x;
+	temp->y=y;
+	temp->next=head;
+	head=temp;
+}
+
+void DelFirst()
+{
+	if(head!=NULL)
+	{
+		struct Point *temp=head;
+		head=head->next;
+		free(temp);
+		cout<<0<<endl;
+	}
+	else 
+		cout<<-1<<endl;
+}
+
+void Del(int x,int y)
+{
+	struct Point *temp=head,*prev=NULL;
+	while(temp!=NULL)
+	{
+		if(temp->x==x&&temp->y==y)
+		{
+			if(prev==NULL)
+				head=temp->next;
+			else 
+				prev->next=temp->next;
+			free(temp);
+			cout<<0<<endl;
+			return;
+		}
+		prev=temp;
+		temp=temp->next;
+	}
+	cout<<-1<<endl;
+
+}
+
+int length()
+{
+	struct Point *temp=head;
+	int c=0;
+	while(temp!=NULL)
+	{
+		c++;
+		temp=temp->next;
+	}
+	return c;
+}
+
+
+bool search(int x,int y)
+{
+	struct Point *temp=head;
+	while(temp!=NULL)
+	{
+		if(temp->x==x && temp->y==y)
+			return true;
+		temp=temp->next;
+	}
+	return false;
+}
+
+
+void search(float d)
+{
+	struct Point *temp=head;
+	if(head==NULL)
+		cout<<-1<<endl;
+	else
+	{
+		while(temp!=NULL)
+		{
+			float f=(temp->x*temp->x + temp->y*temp->y);
+			if(f<=d*d)
+				cout<<"("<<temp->x<<","<<temp->y<<") ";
+			temp=temp->next;
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,x,y;
+		float d;
+		cin>>n;
+		switch(n)
+		{
+			case 1:
+			cin>>x>>y;
+			AddFirst(x,y);
+			break;
+			case 2:
+			DelFirst();
+			break;
+			case 3:
+			cin>>x>>y;
+			Del(x,y);
+			break;
+			case 4:
+			cin>>d;
+			search(d);
+			break;
+			case 5:
+			cin>>x>>y;
+			if(search(x,y))
+			cout<<"True"<<endl;
+			if(!(search(x,y)))
+			cout<<"False"<<endl;
+			break;
+			case 6:
+			cout<<length()<<endl;
+			break;
+		}
+	}
+	return 0;
+}
