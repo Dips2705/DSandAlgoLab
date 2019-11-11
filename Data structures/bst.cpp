475edd38dffa445cc507fca8992bcd9a984a8d7b
Dips2705 <rganguly143@gmail.com>
2019-11-11 16:28:04 +0530

    lab 13 uploaded

--- Data structures/bst.cpp
+++ Data structures/bst.cpp
@@ -1,132 +1,108 @@
-#include<iostream>
-#include<conio.h>
-#include<stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-void insert(int,int );
-void delte(int);
-void display(int);
-int search(int);
-int search1(int,int);
-int tree[40],t=1,s,x,i;
- 
-main()
-{
-    int ch,y;
-    for(i=1;i<40;i++)
-    tree[i]=-1;
-    while(1)
+#define M 1000000007
+#define ll long long
+#define REP(i,a,b) for(ll i=a;i<b;i++)
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<long long>
+struct node{
+    int data;
+    node* left;
+    node* right;
+};
+struct node *newNode(int item) 
+{ 
+    struct node *temp =  new struct node; 
+    temp->data = item; 
+    temp->left = temp->right = NULL; 
+    return temp; 
+} 
+
+
+void inorder(struct node*root)
     {
-cout <<"1.INSERT\n2.DELETE\n3.DISPLAY\n4.SEARCH\n5.EXIT\nEnter your choice:";
-        cin >> ch;
-        switch(ch)
+        if(root)
         {
-        case 1:
-            cout <<"enter the element to insert";
-            cin >> ch;
-            insert(1,ch);
-            break;
-        case 2:
-            cout <<"enter the element to delete";
-            cin >>x;
-            y=search(1);
-            if(y!=-1) delte(y);
-            else cout<<"no such element in tree";
-            break;
-        case 3:
-            display(1);
-            cout<<"\n";
-            for(int i=0;i<=32;i++)
-            cout <<i;
-            cout <<"\n";
-            break;
-case 4:
-            cout <<"enter the element to search:";
-            cin >> x;
-            y=search(1);
-            if(y == -1) cout <<"no such element in tree";
-            else cout <<x << "is in" <<y <<"position";
-            break;
-        case 5:
-            exit(0);
+            inorder(root->left);    //Go to left subtree
+            printf("%d ",root->data);    //Printf root->data
+            inorder(root->right);     //Go to right subtree
         }
     }
-}
- 
-void insert(int s,int ch )
-{
-    int x;
-    if(t==1)
+struct node* insert(struct node* root, int data)
     {
-        tree[t++]=ch;
-        return;
+        if (root == NULL) 
+            return newNode(data);
+        else
+        {
+          
+            if (data <= root->data)
+                root->left  = insert(root->left, data);
+            else
+                root->right = insert(root->right, data);
+            
+            return root;
+        }
     }
-    x=search1(s,ch);
-    if(tree[x]>ch)
-        tree[2*x]=ch;
-    else
-        tree[2*x+1]=ch;
-    t++;
+struct node * successor(struct node* node) 
+{ 
+    struct node* current = node; 
+    while (current && current->left != NULL) 
+        current = current->left; 
+    return current; 
 }
-void delte(int x)
-{
-    if( tree[2*x]==-1 && tree[2*x+1]==-1)
-        tree[x]=-1;
-    else if(tree[2*x]==-1)
-          { tree[x]=tree[2*x+1];
-        tree[2*x+1]=-1;
-          }
-    else if(tree[2*x+1]==-1)
-          { tree[x]=tree[2*x];
-        tree[2*x]=-1;
-          }
+struct node* deleteNode(struct node* root, int key) 
+{ 
+    if (root == NULL) return root; 
+    if (key < root->data) 
+        root->left = deleteNode(root->left, key); 
+    else if (key > root->data) 
+        root->right = deleteNode(root->right, key); 
     else
-    {
-      tree[x]=tree[2*x];
-      delte(2*x);
+    { 
+        if (root->left == NULL) 
+        { 
+            struct node *temp = root->right; 
+            free(root); 
+            return temp; 
+        } 
+        else if (root->right == NULL) 
+        { 
+            struct node *temp = root->left; 
+            free(root); 
+            return temp; 
+        } 
+        struct node* temp = successor(root->right); 
+        root->data = temp->data; 
+        root->right = deleteNode(root->right, temp->data); 
+    } 
+    return root; 
+}
+int main(){
+    struct node *root = NULL;
+    int t;
+    cin>>t;
+    cout<<"Enter 1 to insert:\n";
+    cout<<"Enter 2 to delet: \n";
+    cout<<"Enter 3 to print inorder:\n";
+    while(t--){
+        int a;
+        cin>>a;
+        if(a == 1){
+            int b;
+            cin>>b;
+            root = insert(root, b);
+        }
+        else if(a == 2){
+            int b;
+            cin>>b;
+            root = deleteNode(root, b);
+        }
+        else{
+            inorder(root);
+        }
     }
-    t--;
-}
- 
-int search(int s)
-{
-if(t==1)
-{
-cout <<"no element in tree";
-return -1;
-}
-if(tree[s]==-1)
-return tree[s];
-if(tree[s]>x)
-search(2*s);
-else if(tree[s]<x)
-search(2*s+1);
-else
-return s;
-}
- 
-void display(int s)
-{
-if(t==1)
-{cout <<"no element in tree:";
-return;}
-for(int i=1;i<40;i++)
-if(tree[i]==-1)
-cout <<" ";
-else cout <<tree[i];
-return ;
-}
- 
-int search1(int s,int ch)
-{
-if(t==1)
-{
-cout <<"no element in tree";
-return -1;
-}
-if(tree[s]==-1)
-return s/2;
-if(tree[s] > ch)
-search1(2*s,ch);
-else search1(2*s+1,ch);
 }
